94557c2c7c46461dee477193e1d7ddaf546dd2ee
Nikol_Niven <ntopalusic@gmail.com>
2025-01-28 21:31:35 +0100

    mini changes on sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,19 @@
 import styles from "./Sidebar.module.css";
 
 export default function Sidebar() {
-    return (
-        <aside className={styles.sidebar}>
-        <h2>Sidebar</h2>
-        </aside>
-    );
+  return (
+    <aside className={styles.sidebar}>
+      <h2>SidebarrA</h2>
+      
+        <ul className = {styles.sideBarElem}>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+        </ul>
+      
+    </aside>
+  );
 }
