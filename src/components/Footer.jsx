96959172d230c5aa1398a2c7a151aec2460c5ba6
MrMatroxa <matiss.uskans@gmail.com>
2025-01-28 18:23:33 +0100

    skeleton

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+  return <footer className="footer">
+    <a href="https://github.com/MrMatroxa/react-app" target="_blank">GitHub repo</a>
+  </footer>;
+}
