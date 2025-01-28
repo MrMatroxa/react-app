504bbe3ccfbf109e1f4d12ff6b3e3b11dd564b70
MrMatroxa <matiss.uskans@gmail.com>
2025-01-28 19:05:37 +0100

    skeleton added header, footer, sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,10 @@
-import styles from './Navbar.module.css';
+import styles from "./Navbar.module.css";
+import logo from "../assets/logo.png";
 export default function Navbar() {
-    return <nav className={styles.navbar}>
-        <a href="/">Home</a>
-        </nav>
+  return (
+    <nav className={styles.navbar}>
+      <img src={logo} alt="website logo" className={styles.logo} />
+      <h1>Do It!!!!</h1>
+    </nav>
+  );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,9 @@
+import styles from "./Sidebar.module.css";
+
+export default function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+        <h2>Sidebar</h2>
+        </aside>
+    );
+}
