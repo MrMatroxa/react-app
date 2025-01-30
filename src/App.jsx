8358d7a5a913ba4ada90e25f6e32c8bae53bf1fd
MrMatroxa <matiss.uskans@gmail.com>
2025-01-30 15:16:51 +0100

    made app.jsx imports and component files + css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
 import Footer from "./components/Footer";
-import List from "./components/list";
+import List from "./components/List";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import AboutPage from "./components/AboutPage";
+import NotFoundPage from "./components/NotFoundPage";
+import Dashboard from "./components/Dashboard";
+import data from "./assets/data.json";
 
+import { Router, Route, useLocation } from "react-router-dom";
 
 function App() {
   return (
