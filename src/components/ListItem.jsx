3c2c0a3623ebbbaf96d57578b9f35a71459ebe6a
MrMatroxa <matiss.uskans@gmail.com>
2025-02-03 16:16:01 +0100

    minor change

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,10 +1,17 @@
-import React, { useState } from 'react';
-import styles from './ListItem.module.css';
-import yes from '../assets/check_mark.png'; 
-import no from '../assets/x.png'; 
+import React, { useState } from "react";
+import styles from "./ListItem.module.css";
+import yes from "../assets/check_mark.png";
+import no from "../assets/x.png";
 import { Link } from "react-router-dom";
 
-export default function ListItem({ item, onEdit, toggleCompletion, handleDelete, completed, toggleEdit }) {
+export default function ListItem({
+  item,
+  onEdit,
+  toggleCompletion,
+  handleDelete,
+  completed,
+  toggleEdit,
+}) {
   const [newText, setNewText] = useState(item.task);
 
   const handleSubmit = (e) => {
@@ -26,20 +33,20 @@ export default function ListItem({ item, onEdit, toggleCompletion, handleDelete,
       ) : (
         <>
           <img
-        src={item.completed ? yes : no}
-        onClick={() => toggleCompletion(item.id)}
-        alt="Completion status"
-      />
-      <Link to={`/item/${item.id}`} className={styles.listItemP}>
-        <p>Task: {item.task}</p>
-      </Link>
+            src={item.completed ? yes : no}
+            onClick={() => toggleCompletion(item.id)}
+            alt="Completion status"
+          />
+          <Link to={`/item/${item.id}`} className={styles.listItemP}>
+            <p>{item.task}</p>
+          </Link>
           <button onClick={toggleEdit}>Edit</button>
           <button
-        className={styles.deleteButton}
-        onClick={() => handleDelete(item.id)}
-      >
-        Delete
-      </button>
+            className={styles.deleteButton}
+            onClick={() => handleDelete(item.id)}
+          >
+            Delete
+          </button>
         </>
       )}
     </div>
